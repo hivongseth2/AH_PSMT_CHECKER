c9c01a220141492cedf2feb046d31af4832bba21
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2025-03-21 16:51:05 +0700

    on roi, truoc khi add router

--- src/components/UserGuide/tabs.jsx
+++ src/components/UserGuide/tabs.jsx
@@ -68,7 +68,7 @@ export function TabsTrigger({ value, className, children, ...props }) {
       data-state={isSelected ? "active" : "inactive"}
       className={`inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${
         isSelected
-          ? "bg-background text-foreground shadow-sm"
+        ? "bg-blue-50 text-blue-700 shadow-sm  border-b-2 border-blue-400"
           : "text-muted-foreground hover:bg-muted hover:text-foreground"
       } ${className}`}
       onClick={() => context.onValueChange(value)}
